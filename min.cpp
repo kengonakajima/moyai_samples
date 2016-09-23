3902417dacca83ea148b69e54656e430a2b63252
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-09-23 09:35:13 +0900

    1line prop

--- min.cpp
+++ min.cpp
@@ -1,6 +1,8 @@
 #include "sample_common.h"
 
 void minimumInit() {
+    Prop2D *p = new Prop2D(g_base_deck,ATLAS_MYSHIP,48,Vec2(0,0));
+    g_main_layer->insertProp(p);
 }
 void minimumUpdate() {
 }
